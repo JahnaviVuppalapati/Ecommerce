0871fffaf5381537f1649255804718f79378d1b8
Jahnavi <jahnavi13042004@gmail.com>
2025-03-28 02:49:51 +0530

    Setting up MongoDB connection and DataBase

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -8,6 +9,30 @@ app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 8080;
 
+//mongodb connection
+console.log(process.env.MONGODB_URL)
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connect to Databse"))
+  .catch((err) => console.log(err))
+
+//schema
+const userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: String,
+    confirmPassword: String,
+    image: String,
+  })
+
+//model
+const userModel = mongoose.model("user", userSchema);
+//api
 app.get("/", (req, res) => {
     res.send("Server is running");
   })
