c7892ac187c31bc630546914dc53dd55347a8fa4
Jahnavi <jahnavi13042004@gmail.com>
2025-04-02 00:06:31 +0530

    restrict newproject.js visibility to admin

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -20,6 +20,7 @@ const Header = () => {
     dispatch(logoutRedux());
     toast("logged out succesfully");
   };
+  console.log(process.env.REACT_APP_ADMIN_EMAIL)
 
   return (
     <header className="fixed shadow-md w-full h-16 px-2 md:px-4 z-50 bg-white">
@@ -58,15 +59,15 @@ const Header = () => {
 
             {showMenu && (
               <div className="absolute right-2 bg-white py-2  shadow drop-shadow-md flex flex-col min-w-[120px] text-center">
-                {/* {userData.email === process.env.REACT_APP_ADMIN_EMAIL && ( */}
+                {userData.email === process.env.REACT_APP_ADMIN_EMAIL && (
                   <Link
                     to={"newproduct"}
                     className="whitespace-nowrap cursor-pointer px-2"
                   >
                     New product
                   </Link>
-                 {/* ) 
-                 }  */}
+                  ) 
+                 } 
 
                  {userData.image ? (
                   <p
